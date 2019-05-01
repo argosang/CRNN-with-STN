7cb7f4e6f6307483981c3f4956bf4e308c3ba067
argosang <47573937+argosang@users.noreply.github.com>
2019-05-01 11:44:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -8,19 +8,19 @@ characters = '0123456789'+string.ascii_lowercase+'-'
 label_classes = len(characters)+1
 
 # for batch_generator
-lexicon_dic_path = '/media/junbo/DATA/OCR_datasets/max/lexicon.txt'
-file_list = open('/media/junbo/DATA/OCR_datasets/max/annotation_train.txt', 'r')
-file_list_val = open('/media/junbo/DATA/OCR_datasets/max/annotation_val.txt', 'r')
-img_folder = '/media/junbo/DATA/OCR_datasets/max/90kDICT32px'
+lexicon_dic_path = 'mnt/ramdisk/max/90kDICT32px/lexicon.txt'
+file_list = open('mnt/ramdisk/max/90kDICT32px/annotation_train.txt', 'r')
+file_list_val = open('mnt/ramdisk/max/90kDICT32px/annotation_val.txt', 'r')
+img_folder = 'mnt/ramdisk/max/90kDICT32px/'
 
 
 # for CRNN_with_STN
 learning_rate = 0.0001  # learning rate, 0.002 for default
-cp_save_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_best_STN.{epoch:02d}-{loss:.2f}.hdf5' \
+cp_save_path = 'weights_best_STN.{epoch:02d}-{loss:.2f}.hdf5' \
     # save checkpoint path
-base_model_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_for_predict_STN.hdf5'  \
+base_model_path = 'weights_for_predict_STN.hdf5'  \
     # the model for predicting
-tb_log_dir = '/home/junbo/PycharmProjects/test0_mnist/CRC_n/paper_log'  # TensorBoard save path, Optional
+tb_log_dir = 'paper_log'  # TensorBoard save path, Optional
 load_model_path = ''  \
     # if you want to train a new model, please set  load_model_path = ""
 
